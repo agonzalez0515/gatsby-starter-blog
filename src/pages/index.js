e47307c5ecf4c0414711c949f165d481c2c462b9
Angelica Gonzalez <agonzalez0515@gmail.com>
2019-05-09 22:32:43 -0700

    remove description again

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,7 +22,7 @@ class BlogIndex extends React.Component {
               <small>{node.frontmatter.date}</small>
               <p
                 dangerouslySetInnerHTML={{
-                  __html: node.frontmatter.description || node.excerpt,
+                  __html: node.excerpt,
                 }}
               />
             </div>
@@ -47,7 +47,6 @@ export const pageQuery = graphql`
           frontmatter {
             date(formatString: "MMMM DD, YYYY")
             title
-            description
           }
         }
       }
